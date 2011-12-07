e310eadc5b1a2a348595901329fc37c2f6ed031d
Niroj Shrestha <niroj77@yahoo.com>
2011-12-07 10:11:08 +0545

    login spec finished

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,14 +1,14 @@
-# FactoryGirl.define do
- #  sequence :email do |n|
- #     "person#{n}@example.com"
- #   end
- # end
- #       
+FactoryGirl.define do
+  sequence :email do |n|
+     "person#{n}@example.com"
+   end
+ end
+        
 
 FactoryGirl.define do
 
   factory :user do
-    email "abc@yahoo.com"
-    password "secret"
+    email
+    password "anything"
   end
 end
---
+++ spec/requests/login_spec.rb
@@ -0,0 +1,83 @@
+require 'spec_helper'
+
+describe "log_in" do
+
+  it "should allow validated user to sign in" do
+    
+    user = Factory(:user) 
+    
+    visit "/users/sign_in"
+    fill_in "Email", :with => "person1@example.com"
+    fill_in "Password", :with => "anything" 
+    click_button "Sign in"
+
+    page.should have_content("Signed in successfully.")
+
+  end 
+  
+  it "shouldnot allow invalid password" do
+    
+    user = Factory(:user) 
+    
+    visit "/users/sign_in"
+    fill_in "Email", :with => "person1@example.com"
+    fill_in "Password", :with => "invalidPassword" 
+    click_button "Sign in"
+
+    page.should have_content("Invalid email or password.")
+
+  end  
+  
+  it "should not allow invalid email" do
+    
+    user = Factory(:user) 
+    
+    visit "/users/sign_in"
+    fill_in "Email", :with => "notaemail@example.com"
+    fill_in "Password", :with => "anything" 
+    click_button "Sign in"
+
+    page.should have_content("Invalid email or password.")
+
+  end
+  
+  it "should allow blank email" do
+    
+    user = Factory(:user) 
+    
+    visit "/users/sign_in"
+    fill_in "Email", :with => ""
+    fill_in "Password", :with => "anything" 
+    click_button "Sign in"
+
+    page.should have_content("Invalid email or password.")
+
+  end 
+  
+  it "should allow blank password" do
+    
+    user = Factory(:user) 
+    
+    visit "/users/sign_in"
+    fill_in "Email", :with => "person1@example.com"
+    fill_in "Password", :with => "" 
+    click_button "Sign in"
+
+    page.should have_content("Invalid email or password.")
+
+  end 
+  
+  it "should allow blank email and password" do
+    
+    user = Factory(:user) 
+    
+    visit "/users/sign_in"
+    fill_in "Email", :with => ""
+    fill_in "Password", :with => "" 
+    click_button "Sign in"
+
+    page.should have_content("Invalid email or password.")
+
+  end
+  
+end
