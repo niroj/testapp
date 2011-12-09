65f61002d07133aa927bcafba3a1c500dbf0c521
Niroj Shrestha <niroj77@yahoo.com>
2011-12-09 17:29:10 +0545

    mailer completed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,25 +1,30 @@
-class ReviewsController < ApplicationController 
+class ReviewsController < ApplicationController
   before_filter :find_destination
   before_filter :authenticate_user!
 
   # POST /destinations/:destination_id/reviews
   def create
-    if @destination.user_id != current_user.id	  
-      @review = @destination.reviews.create(params[:review ])  
+    if @destination.user_id != current_user.id
+      @review = @destination.reviews.create(params[:review ])
       @review.user_id = current_user.id
       @review.save
       redirect_to destination_path(@destination)
     else
-      redirect_to destination_path(@destination) 
+      redirect_to destination_path(@destination)
       flash[:alert] = "You Are not allowed to add review in destinations you add"
     end
 
-  end    
-  
+    respond_to do |format|
+      format.html # show.html.erb
+      format.js
+    end
+
+  end
+
 
   private
 
   def find_destination
-    @destination = Destination.find(params[:destination_id])	
-  end    
+    @destination = Destination.find(params[:destination_id])
+  end
 end
